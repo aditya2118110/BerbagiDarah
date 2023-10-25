c37ef5cf90147c14507d98a2389cde4c329a22a6
aditya2118110 <2118110@scholar.itn.ac.id>
2023-10-25 12:56:23 +0700

    [Bab 3] State dan Props

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import { Receipt21 , Notification, HambergerMenu, SearchNormal1, RowHorizontal, Home, Message, Profile, Star1} from 'iconsax-react-native';
 
 import DonorCounter from './src/commponent/DonorCounter';
@@ -9,72 +9,11 @@ import { fontType } from './src/theme';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <HambergerMenu style={{color: 'white'}}></HambergerMenu>
-        <Text style={styles.title}>BerbagiDarah</Text>
-        <Receipt21 variant="Linear" size={20} />
-        <Notification style={{color:'white'}}></Notification>
-      </View>
-      <View style={styles.container2}>
-      <Image
-        source={{
-          uri: 'https://pp.walk.sc/tile/e/0/748x600/loc/lat=-7.978773/lng=112.618375.png', // Ganti dengan URL logo yang sesuai
-        }}
-        style={styles.logo}
-      />
-      </View>
-      <View style={styles.container3}>
-      <Text style={{color:'white', paddingTop: 20, paddingLeft: 20 ,}}>Mencari Donor</Text>
-      <TextInput placeholder='Cari Lokasi' style={styles.container4}></TextInput>
-      <SearchNormal1 style={{color: 'black', marginTop: -32, marginLeft: 365}}></SearchNormal1>
-      <View style={styles.container5}></View>
-      <Text style={{color:'white', paddingTop: 5, paddingLeft: 20 ,}}>Golongan Darah</Text>
-      <View style={styles.container6}>
-      <Image
-        source={{
-          uri: 'https://cdn.pixabay.com/photo/2017/08/22/11/54/blood-group-2668685_1280.png', // Ganti dengan URL logo yang sesuai
-        }}
-        style={{width:100,height: 150}}/>
-      <Image
-        source={{
-          uri: 'https://cdn.pixabay.com/photo/2017/08/22/11/56/blood-group-2668698_1280.png', // Ganti dengan URL logo yang sesuai
-        }}
-        style={{width:100,height: 150}}/>
-        <Image
-        source={{
-          uri: 'https://cdn.pixabay.com/photo/2017/08/22/11/55/blood-group-2668693_1280.png', // Ganti dengan URL logo yang sesuai
-        }}
-        style={{width:100,height: 150}}/>
-        <Image
-        source={{
-          uri: 'https://cdn.pixabay.com/photo/2017/08/22/11/54/blood-group-2668683_1280.png', // Ganti dengan URL logo yang sesuai
-        }}
-        style={{width:100,height: 150}}/>
-      </View>
-      <View style={styles.container7}></View>
-      <View style={styles.container6}>
-        <Text style={{color:'white', paddingTop: 20, paddingLeft: 125,}}>Rhesus</Text>
-      </View>
-      <View style={styles.container8}></View>
-        <TouchableOpacity>
-      <View style={styles.container9}>
-
-      <Text style={{color:'black', paddingTop: 10, paddingLeft: 65,fontFamily: fontType['Pjs-Bold'], fontSize: 20}}>Cari</Text>
-      </View>
-        </TouchableOpacity>
-      <View style={styles.container10}>
-      <Home style={{color: 'black', marginTop: 22, marginLeft: 30}}></Home>
-      <SearchNormal1 style={{color: 'black', marginTop: 22, marginLeft: 60}}></SearchNormal1>
-      <Message style={{color: 'black', marginTop: 22, marginLeft: 60}}></Message>
-      <Star1 style={{color: 'black', marginTop: 22, marginLeft: 70}}></Star1>
-      <Profile style={{color: 'black', marginTop: 22, marginLeft: 50}}></Profile>
-      </View>
-      </View>
-      <DonorCounter donorCount={10} />
-      <DonorRequirement requiredDonors={50} />
-      {/* <ListBlog /> */}
+    <ScrollView>
+    <View>
+      <ListBlog />
     </View>
+    </ScrollView>
   );
 };
 
--- src/ListBlog.js
+++ src/ListBlog.js
@@ -1,37 +1,123 @@
-// ListBlog.js
-import React from 'react';
-import { ScrollView, View, Text, StyleSheet, ImageBackground } from 'react-native';
-import { Receipt21 } from 'iconsax-react-native';
-import {colors} from './theme/colors';
+import React, { useState } from 'react';
+import { ScrollView, View, Text, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
+import { Receipt21, Like1, Message } from 'iconsax-react-native';
+import { colors } from '../src/theme';
 
 const ListBlog = () => {
+  const [likes, setLikes] = useState(0);
+  const [messages, setMessages] = useState(0);
+  const [likeColor, setLikeColor] = useState('white');
+  const [messageColor, setMessageColor] = useState('white');
+
+  const contentData = [
+    {
+      description: 'Palang Merah Indonesia (PMI) Kota Malang menggelar apel bersama dalam rangka HUT ke-78 Markas PMI Kota Malang.',
+      image: 'https://malangkota.go.id/wp-content/uploads/2023/09/Wali-Kota-Malang-menjadi-anggota-kehormatan-PMI-Kota-Malang.jpeg', // Ganti dengan URL gambar yang sesuai
+    },
+    {
+      description: 'Palang Merah Indonesia (PMI) Kota Malang menggelar apel bersama dalam rangka HUT ke-78 Markas PMI Kota Malang.',
+      image: 'https://malangkota.go.id/wp-content/uploads/2023/09/Wali-Kota-Malang-menjadi-anggota-kehormatan-PMI-Kota-Malang.jpeg', // Ganti dengan URL gambar yang sesuai
+    },
+    {
+      description: 'Palang Merah Indonesia (PMI) Kota Malang menggelar apel bersama dalam rangka HUT ke-78 Markas PMI Kota Malang.',
+      image: 'https://malangkota.go.id/wp-content/uploads/2023/09/Wali-Kota-Malang-menjadi-anggota-kehormatan-PMI-Kota-Malang.jpeg', // Ganti dengan URL gambar yang sesuai
+    },
+    {
+      description: 'Palang Merah Indonesia (PMI) Kota Malang menggelar apel bersama dalam rangka HUT ke-78 Markas PMI Kota Malang.',
+      image: 'https://malangkota.go.id/wp-content/uploads/2023/09/Wali-Kota-Malang-menjadi-anggota-kehormatan-PMI-Kota-Malang.jpeg', // Ganti dengan URL gambar yang sesuai
+    },
+    {
+      description: 'Palang Merah Indonesia (PMI) Kota Malang menggelar apel bersama dalam rangka HUT ke-78 Markas PMI Kota Malang.',
+      image: 'https://malangkota.go.id/wp-content/uploads/2023/09/Wali-Kota-Malang-menjadi-anggota-kehormatan-PMI-Kota-Malang.jpeg', // Ganti dengan URL gambar yang sesuai
+    },
+  ];
+
   return (
-    <ScrollView>
-      <View style={styles.listBlog}>
-        {/* Donor Counter */}
-        <DonorCounter donorCount={10} />
-
-        {/* Donor Requirement */}
-        <DonorRequirement requiredDonors={50} />
-
-        {/* Blog Post 1 */}
-        <View style={itemHorizontal.cardItem}>
+    <ScrollView style={styles.container}>
+      {contentData.map((content, index) => (
+        <View style={styles.blogItem} key={index}>
           <ImageBackground
-            style={itemHorizontal.cardImage}
-            resizeMode="cover"
-            imageStyle={{ borderRadius: 15 }}
             source={{
-              uri: 'https://images.unsplash.com/photo-1593941707882-a5bba14938c7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1744&q=80',
-            }}>
-            {/* Blog content */}
+              uri: content.image,
+            }}
+            style={styles.image}
+          >
+            <Text style={styles.title}>{content.title}</Text>
           </ImageBackground>
-        </View>
+          <Text style={styles.description}>{content.description}</Text>
 
-        {/* Blog Post 2 */}
-        {/* ... (similar structure for other blog posts) */}
-      </View>
+          <View style={styles.iconContainer}>
+            <TouchableOpacity
+              onPress={() => {
+                setLikes(likes + 1);
+                setLikeColor('red');
+              }}
+              style={styles.iconButton}
+            >
+              <Like1 style={[styles.icon, { color: likeColor }]} />
+              <Text>{likes}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => {
+                setMessages(messages + 1);
+                setMessageColor('green');
+              }}
+              style={styles.iconButton}
+            >
+              <Message style={[styles.icon, { color: messageColor }]} />
+              <Text>{messages}</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ))}
     </ScrollView>
   );
 };
 
-const styles = StyleSheet
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  blogItem: {
+    margin: 10,
+    borderRadius: 10,
+    overflow: 'hidden',
+    elevation: 5,
+  },
+  image: {
+    height: 200,
+    width : 365,
+    marginLeft: 20,
+    marginTop: 20,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    color: colors.white,
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    padding: 10,
+  },
+  description: {
+    fontSize: 16,
+    margin: 10,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  iconButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: 5,
+    color: 'white',
+  },
+});
+
+export default ListBlog;
